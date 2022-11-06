14f5a2f28be3e1e61419659ce546dd37a99b8005
Amrit Bastakoti <amritbastakoti618@gmail.com>
2022-11-06 17:46:18 +0200

    necesary dependencies

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require ("express")
 const app = express();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+const helmet = require("hemet")
+const morgan = require("morgan")
+
+dotenv.config()
 
 app.listen(8800, () => {
     console.log("Backend server is running!")
